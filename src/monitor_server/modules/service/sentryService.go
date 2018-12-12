1a372b5b335124075992d5ee67e0d9342f672868
yaoxf <850630887@qq.com>
2018-12-12 23:00:48 +0800

    报警系统完成

--- src/monitor_server/modules/service/sentryService.go
+++ src/monitor_server/modules/service/sentryService.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"monitor_server/components"
 	. "monitor_server/tools"
-    _"github.com/astaxie/beego/logs"
+    "github.com/astaxie/beego/logs"
 	simplejson "github.com/bitly/go-simplejson"
 )
 
@@ -50,28 +50,33 @@ func (this SentryService) EventCheck(c *gin.Context) {
     var tmpKey string = ""
 	for _, row := range rows {
 		if each_map, ok := row.(map[string]interface{}); ok {
+            // 判断是否已经发过报警信息
             tmpKey = fmt.Sprintf("%s%s", key, each_map["eventID"].(string))
-            tmpRes := RedisHandleObj.GetString(tmpKey)
-            fmt.Println(tmpRes)
+            tmpRes := components.RedisHandleObj.GetString(tmpKey)
+            if tmpRes == "ok" {
+                continue
+            }
 
-			fmt.Println(each_map["eventID"])
 			if each_map2, ok2 := each_map["entries"].([]interface{}); ok2 {
 				for _, v := range each_map2 {
 					if each_map3, ok3 := v.(map[string]interface{}); ok3 {
 						if each_map4, ok4 := each_map3["data"].(map[string]interface{}); ok4 {
 							if tmpMsg, ok5 := each_map4["message"]; ok5 {
-								fmt.Println(tmpMsg)
+                                // 发送告警消息
+                                _, err := components.SendWxMsg("@all", tmpMsg.(string))
+                                if err != nil {
+                                    logs.Error(1024, err.Error())
+                                }
+
+                                // 标志已经发送过了
+                                components.RedisHandleObj.Set(tmpKey, "ok", 864000)
 							}
 						}
 					}
 				}
 			}
 		}
-
-
-		return
 	}
-
 
 	ApiReturnSuccess(c, 0, "success")
 }
